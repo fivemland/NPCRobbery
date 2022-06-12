666082ec118168d50a0a835d3979d3fcf1808cb8
MedveMarci <martonmende@gmail.com>
2022-06-12 15:58:13 +0200

    Randomised NPC Spawn

--- client.lua
+++ client.lua
@@ -1,88 +1,83 @@
-
 ESX = nil
 
 Citizen.CreateThread(function()
-    while ESX == nil do
-      TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-      Citizen.Wait(0)
-    end
-  end) 
- 
-  Citizen.CreateThread(function()
-      RequestModel(GetHashKey(Config.Ped))
-      while not HasModelLoaded(GetHashKey(Config.Ped)) do
-          Wait(1)
-      end
-    local npc = CreatePed(4, 0x62CC28E2, Config.Coords, false, true)
-    FreezeEntityPosition(npc, true)	
-    SetEntityHeading(npc, 92.96)
-    SetEntityInvincible(npc, true)
-    SetBlockingOfNonTemporaryEvents(npc, true)
-  end)
-  
-function Random()
-    local number = math.random(0, 1)
-if number == 1 then  
-  print(1)
-end
-if number == 0 then  
-    print(0)
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(0)
   end
+end) 
 
+Citizen.CreateThread(function()
+    RequestModel(GetHashKey(Config.Ped))
+    while not HasModelLoaded(GetHashKey(Config.Ped)) do
+        Wait(1)
+    end
+  local npc = CreatePed(4, 0x62CC28E2, Config.Coords, false, true)
+  FreezeEntityPosition(npc, true)	
+  SetEntityHeading(npc, 92.96)
+  SetEntityInvincible(npc, true)
+  SetBlockingOfNonTemporaryEvents(npc, true)
+end)
 
-   --[[ if number = 0
-      exports.pNotify:SendNotification({
-        text = 'Egy pillanat és kijelölöm a térképen a túsz helyét!',
-        type = "error",
-        timeout = 5000,
-        layout = "topCenter",
-    })
-        Citizen.Wait(10000)
-        RequestModel(GetHashKey(Config.HostagePed))
-        while not HasModelLoaded(GetHashKey(Config.HostagePed)) do
-          Wait(1)
-        end
-      local npc = CreatePed(4, 0x62CC28E2, Config.CoordsToSpawn, false, true)
-      BlipNPC = AddBlipForEntity(npc)
-      SetBlipDisplay(BlipNPC, 4)
-      SetBlipScale  (BlipNPC, 0.8)
-      SetBlipColour (BlipNPC, 0)
-      SetBlipAsShortRange(BlipNPC, true)
-      BeginTextCommandSetBlipName("STRING")
-      AddTextComponentString('Túsz')
-      EndTextCommandSetBlipName(BlipNPC)
-      TaskPedSlideToCoord(npc, Config.CoordsToWalk, 1000)
-        if number then
-          print('fasz vagy')
+RegisterNetEvent('NPCSpawn')
+AddEventHandler('NPCRobStart', function()
+    exports.pNotify:SendNotification({
+      text = 'Egy pillanat és kijelölöm a térképen a túsz helyét!',
+      type = "error",
+      timeout = 5000,
+      layout = "topCenter",
+      })
+      Citizen.Wait(10000)
+      RequestModel(GetHashKey(Config.HostagePed))
+      while not HasModelLoaded(GetHashKey(Config.HostagePed)) do
+        Wait(1)
       end
-    end]]
-end
-  
-  RegisterNetEvent('Riasztás')
-  AddEventHandler('RiasztásPD', function()
-  print('ANYA')
-  end)
-  
-  AddEventHandler('rablas', function()
-  NPCRobStart()
-  end)
-  
-  
-  exports.qtarget:AddBoxZone("StartNPCRobbery", Config.ROBLocation.targetZone, 1, 1, {
-    name="StartNPCRobbery",
-    heading = Config.ROBLocation.targetHeading,
-    debugPoly = false,
-    minZ = Config.ROBLocation.minZ,
-    maxZ = Config.ROBLocation.maxZ,
-    }, {
-      options = {
+    local npc = CreatePed(4, 0x62CC28E2, Config.CoordsToSpawn, false, true)
+    BlipNPC = AddBlipForEntity(npc)
+    SetBlipDisplay(BlipNPC, 4)
+    SetBlipScale  (BlipNPC, 0.8)
+    SetBlipColour (BlipNPC, 0)
+    SetBlipAsShortRange(BlipNPC, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString('Túsz')
+    EndTextCommandSetBlipName(BlipNPC)
+
+    Coords = {
+      Config.CoordsToWalk.a,
+      Config.CoordsToWalk.b
+    }
+
+    function RandomCoords()
+    return Coords[math.random(#Coords)]
+    end
+    TaskPedSlideToCoord(npc, RandomCoords(), 1000)
+end)
+
+RegisterNetEvent('Riasztás')
+AddEventHandler('RiasztásPD', function()
+print('ANYA')
+end)
+
+AddEventHandler('rablas', function()
+TriggerEvent('NPCRobStart')
+end)
+
+
+exports.qtarget:AddBoxZone("StartNPCRobbery", Config.ROBLocation.targetZone, 1, 1, {
+   name="StartNPCRobbery",
+   heading = Config.ROBLocation.targetHeading,
+   debugPoly = false,
+   minZ = Config.ROBLocation.minZ,
+   maxZ = Config.ROBLocation.maxZ,
+   }, {
+     options = {
         {
           event = "rablas",
           icon = "Fas Fa-hands",
           label = "Túszok elrablása",
         }
       },
-      distance = 3.5
-  })
+    distance = 3.5
+})
     --Client->Server->polices
    
