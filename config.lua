a0893df1bbcbbe0909b55e16c78f2105a7ae3aeb
MedveMarci <martonmende@gmail.com>
2022-06-12 15:58:24 +0200

    Randomised NPC Spawn

--- config.lua
+++ config.lua
@@ -6,8 +6,10 @@ Config.Coords = vector3(-197.881317, -868.496704, 28.263062)
 
 Config.HostagePed = "s_m_y_armymech_01"
 Config.CoordsToSpawn = vector3(-197.881317, -868.496704, 28.263062)
-Config.CoordsToWalk = vector3(-144.092300, -723.283508, 34.722290)
-Config.CoordsToWalk2 = vector3(-192.685715, -855.784607, 30.846924)
+Config.CoordsToWalk = {
+    a = vector3(-144.092300, -723.283508, 34.722290),
+    b = vector3(-1921.685715, -855.784607, 30.846924)
+}
 
 Config.ROBLocation = {
     targetZone = vector3(-197.881317, -868.496704, 29.863062),
