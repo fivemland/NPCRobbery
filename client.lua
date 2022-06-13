61a0f3222285353fc21f25baeaa2762c8f51fcdc
CsokiHUN <feketetibi71@gmail.com>
2022-06-13 08:53:58 +0200

    Change ped createion while to loadModel(), some refactor

--- client.lua
+++ client.lua
@@ -26,11 +26,9 @@ function loadModel(hash)
 end
 
 CreateThread(function()
-    RequestModel(GetHashKey(Config.Ped))
-    while not HasModelLoaded(GetHashKey(Config.Ped)) do
-        Wait(1)
-    end
-  local npc = CreatePed(4, 0x62CC28E2, Config.Coords, false, true)
+  local hash = loadModel(Config.Ped)
+
+  local npc = CreatePed(4, hash, Config.Coords, false, true)
   FreezeEntityPosition(npc, true)	
   SetEntityHeading(npc, 92.96)
   SetEntityInvincible(npc, true)
@@ -39,51 +37,46 @@ end)
 
 RegisterNetEvent('NPCRobStart')
 AddEventHandler('NPCRobStart', function()
-    exports.pNotify:SendNotification({
-      text = 'Egy pillanat és kijelölöm a térképen a túsz helyét!',
-      type = "error",
-      timeout = 5000,
-      layout = "topCenter",
-      })
-      Wait(10000)
-      RequestModel(GetHashKey(Config.HostagePed))
-      while not HasModelLoaded(GetHashKey(Config.HostagePed)) do
-        Wait(1)
-      end
-    local npc = CreatePed(4, 0x62CC28E2, Config.CoordsToSpawn, false, true)
-    BlipNPC = AddBlipForEntity(npc)
-    SetBlipDisplay(BlipNPC, 4)
-    SetBlipScale  (BlipNPC, 0.8)
-    SetBlipColour (BlipNPC, 0)
-    SetBlipAsShortRange(BlipNPC, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString('Túsz')
-    EndTextCommandSetBlipName(BlipNPC)
+  exports.pNotify:SendNotification({
+    text = 'Egy pillanat és kijelölöm a térképen a túsz helyét!',
+    type = "error",
+    timeout = 5000,
+    layout = "topCenter",
+    })
+  Wait(10000)
 
-    Coords = {
-      Config.CoordsToWalk.a,
-      Config.CoordsToWalk.b
-    }
+  local hash = loadModel(Config.HostagePed)
 
-    function RandomCoords()
-    return Coords[math.random(#Coords)]
-    end
-    TaskPedSlideToCoord(npc, RandomCoords(), 1000)
+  local npc = CreatePed(4, hash, Config.CoordsToSpawn, false, true)
+  BlipNPC = AddBlipForEntity(npc)
+  SetBlipDisplay(BlipNPC, 4)
+  SetBlipScale  (BlipNPC, 0.8)
+  SetBlipColour (BlipNPC, 0)
+  SetBlipAsShortRange(BlipNPC, true)
+  BeginTextCommandSetBlipName("STRING")
+  AddTextComponentString('Túsz')
+  EndTextCommandSetBlipName(BlipNPC)
+
+  local Coords = {
+    Config.CoordsToWalk.a,
+    Config.CoordsToWalk.b
+  }
+
+  TaskPedSlideToCoord(npc, Coords[math.random(#Coords)], 1000)
 end)
 
 AddEventHandler('rablas', function()
-TriggerEvent('NPCRobStart')
+  TriggerEvent('NPCRobStart')
 end)
 
-
 exports.qtarget:AddBoxZone("StartNPCRobbery", Config.ROBLocation.targetZone, 1, 1, {
-   name="StartNPCRobbery",
-   heading = Config.ROBLocation.targetHeading,
-   debugPoly = false,
-   minZ = Config.ROBLocation.minZ,
-   maxZ = Config.ROBLocation.maxZ,
-   }, {
-     options = {
+  name="StartNPCRobbery",
+  heading = Config.ROBLocation.targetHeading,
+  debugPoly = false,
+  minZ = Config.ROBLocation.minZ,
+  maxZ = Config.ROBLocation.maxZ,
+  }, {
+    options = {
         {
           event = "rablas",
           icon = "Fas Fa-hands",
